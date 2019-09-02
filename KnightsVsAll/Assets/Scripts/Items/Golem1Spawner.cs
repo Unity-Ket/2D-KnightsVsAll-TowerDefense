2908b6b4795caf2fda7ee6c275174c3ee7bffadb
Vena Ketpongsuda <>
2019-09-02 01:08:41 -0700

    KnightsVsAll: Animation Event (Reupload)

---
+++ KnightsVsAll/Assets/Scripts/Items/Golem1Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem1Spawner : MonoBehaviour
+{
+    [Range(5f,10f)][SerializeField] float minSpawnDelay;
+    [Range(10f,20f)][SerializeField] float maxSpawnDelay;
+    [SerializeField] Golem1 enemyType;
+
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            spawnEnemy();
+        }
+    }
+
+    private void spawnEnemy()
+    {
+        Instantiate(enemyType, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
